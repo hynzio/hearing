1e6ea25bfa0fb8fd8467b60108999fc75c2e1403
hynzio <hynzzzo813@gmail.com>
2024-11-03 23:21:19 +0900

    new readme file

---
+++ src/main/java/org/hdmd/hearingdemo/HearingdemoApplication.java
@@ -0,0 +1,13 @@
+package org.hdmd.hearingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HearingdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HearingdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hdmd/hearingdemo/HearingdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hdmd.hearingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HearingdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
