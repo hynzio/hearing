08e4e103167e2aa4bb2f2a7cbb1994ffc1bc1be2
hynzio <hynzzzo813@gmail.com>
2024-11-03 23:24:20 +0900

    설정파일들

--- src/main/java/org/hdmd/hearingdemo/HearingdemoApplication.java
+++ src/main/java/org/hdmd/hearingdemo/HearingdemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HearingdemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HearingdemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HearingdemoApplication.class, args);
+    }
 
 }
