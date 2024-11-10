06173223a95d2c13d6f06706360de1a9f7742844
hynzio <hynzzzo813@gmail.com>
2024-11-10 10:16:23 +0900

    MQTT추가/업데이트로직 수정/알림타임스탬프추가

---
+++ src/main/java/org/hdmd/hearingdemo/service/MqttCommandSender.java
@@ -0,0 +1,33 @@
+package org.hdmd.hearingdemo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.mqtt.support.MqttHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MqttCommandSender {
+
+    @Autowired
+    private MessageChannel mqttOutboundChannel;
+
+    public void sendStartCommand() {
+        String command = "start";
+        sendCommand(command);
+    }
+
+    public void sendStopCommand() {
+        String command = "stop";
+        sendCommand(command);
+    }
+
+    private void sendCommand(String command) {
+        Message<String> mqttMessage = MessageBuilder
+                .withPayload(command)
+                .setHeader(MqttHeaders.TOPIC, "raspberry/control")
+                .build();
+        mqttOutboundChannel.send(mqttMessage);
+    }
+}
