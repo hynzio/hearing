e9a6e97bc387cf06e72036189bdaf411383db4f1
hynzio <hynzzzo813@gmail.com>
2024-11-10 16:32:04 +0900

    git

--- src/main/java/org/hdmd/hearingdemo/controller/RecordingController.java
+++ src/main/java/org/hdmd/hearingdemo/controller/RecordingController.java
@@ -1,4 +1,5 @@
 package org.hdmd.hearingdemo.controller;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +25,7 @@ public class RecordingController {
 
     private final RecordingService recordingService;
 
+    // 녹음파일 업로드 처리
     @PostMapping("/recording")
     @Operation(
             summary = "녹음파일 업로드",
@@ -32,13 +34,12 @@ public class RecordingController {
                     @ApiResponse(responseCode = "200", description = "성공적으로 업로드됨"),
                     @ApiResponse(responseCode = "404", description = "존재하지 않는 파일"),
                     @ApiResponse(responseCode = "500", description = "서버 오류")})
-    public Recording processRecording(@RequestBody UploadDTO uploadDTO) throws Exception {
+    public ResponseEntity<Recording> processRecording(@RequestBody UploadDTO uploadDTO) throws Exception {
         Recording recording = recordingService.processRecording(uploadDTO);
-        return ResponseEntity.ok(recording).getBody();
+        return ResponseEntity.ok(recording);
     }
 
-
-    //특정 위험상황 조회
+    // 특정 위험상황 조회
     @GetMapping("/recording/{recordingId}")
     @Operation(
             summary="위험상황데이터 조회",
@@ -48,28 +49,26 @@ public class RecordingController {
                     @ApiResponse(responseCode = "404", description = "존재하지 않는 데이터"),
                     @ApiResponse(responseCode = "500", description = "서버 오류")})
     @Parameter(name = "recordingId", description = "해당하는 녹음파일 id로 데이터 조회")
-    public ResponseEntity<Map<String, Object>>  getRecordingInfo(@PathVariable Long recordingId) throws JsonProcessingException {
+    public ResponseEntity<Map<String, Object>> getRecordingInfo(@PathVariable Long recordingId) throws JsonProcessingException {
         Map<String, Object> response = recordingService.getRecordingInfo(recordingId);
         return ResponseEntity.ok(response);
-
     }
 
-    //보호자 판단 및 기록으로 저장
+    // 보호자 판단 및 기록으로 저장
     @PutMapping("/toHistory")
     @Operation(
             summary = "기록으로 저장",
             description = "해당 데이터를 기록으로 저장",
             responses = {
-                @ApiResponse(responseCode = "200", description = "성공적으로 저장됨"),
-                @ApiResponse(responseCode = "404", description = "존재하지 않는 데이터"),
-                @ApiResponse(responseCode = "500", description = "서버 오류")})
-    public ResponseEntity<?> userReview(
-            @RequestBody RecordingSendDTO recordingSendDTO) throws SQLException {
+                    @ApiResponse(responseCode = "200", description = "성공적으로 저장됨"),
+                    @ApiResponse(responseCode = "404", description = "존재하지 않는 데이터"),
+                    @ApiResponse(responseCode = "500", description = "서버 오류")})
+    public ResponseEntity<String> userReview(@RequestBody RecordingSendDTO recordingSendDTO) throws SQLException {
         recordingService.moveToHistory(recordingSendDTO);
         return ResponseEntity.ok("위험상황이 종료되었습니다.");
-
     }
 
+    // 사용자 검토 결과: 위험 미감지
     @PutMapping("/recording/{recordingId}")
     @Operation(
             summary = "사용자 검토 결과: 위험 미감지",
@@ -79,7 +78,7 @@ public class RecordingController {
                     @ApiResponse(responseCode = "404", description = "존재하지 않는 데이터"),
                     @ApiResponse(responseCode = "500", description = "서버 오류")})
     public ResponseEntity<String> deleteRecording(@PathVariable Long recordingId) {
-            recordingService.notDanger(recordingId);
-            return ResponseEntity.ok("위험하지 않은 데이터로 분류되었습니다.");
+        recordingService.notDanger(recordingId);
+        return ResponseEntity.ok("위험하지 않은 데이터로 분류되었습니다.");
     }
 }
